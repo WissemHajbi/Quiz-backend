f5fe2a6ee8264f24415e4226b54c7cdac48855f3
WissemHajbi <wissemhajbi2002@gmail.com>
2023-02-01 21:09:22 +0100

    Add: model validation

--- Data/Models/QuestionPostRequest.cs
+++ Data/Models/QuestionPostRequest.cs
@@ -1,10 +1,12 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace qAndA.Data.Models {
     public class QuestionPostRequest {
-        public int QuestionId { get; set; }
+        [StringLength(50)]
+        [Required(ErrorMessage = "please enter a correct title")]
         public string Title { get; set; }
+
+        [Required(ErrorMessage = "please enter the content")]
         public string Content { get; set; }
-        public string UserId { get; set; }
-        public string Username { get; set; }
-        public DateTime Created { get; set; }
     }
 }
--- Data/Models/QuestionPutRequest.cs
+++ Data/Models/QuestionPutRequest.cs
@@ -1,5 +1,8 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace qAndA.Data.Models {
     public class QuestionPutRequest {
+        [StringLength(50)]
         public string Title { get; set; }
         public string Content { get; set; }
     }
